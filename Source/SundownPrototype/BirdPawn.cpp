6f7e14933ced8ee9b32000edb7c8aab1c850ab25
Gary Whittle Jr <garywhittle19@gmail.com>
2018-11-21 20:36:02 +0000

    Updated BirdPawn to inherit from ACharacter instead of APawn. Updated flight mechanics for rough feel of pitch limits (can't fly vertically up, can't fly straight down then upside down). Need to revise new mechanics - turning left and right is causing problems with the pitch limits.

--- Source/SundownPrototype/BirdPawn.cpp
+++ Source/SundownPrototype/BirdPawn.cpp
@@ -8,8 +8,6 @@
 #include "Engine/World.h"
 #include "Engine/SkeletalMesh.h"
 
-// SO FAR HAVE LOOKED AT FIXING THE .H AND ACUBECHARACTER()
-
 // Sets default values
 ABirdPawn::ABirdPawn()
 {
@@ -29,18 +27,13 @@ ABirdPawn::ABirdPawn()
 
 	// set our turn rates for input
 	BaseTurnRate = 45.f;
-	BaseLookUpRate = 45.f;
-
-	// Don't rotate when the controller rotates. Let that just affect the camera.
-	//bUseControllerRotationPitch = false;
-	//bUseControllerRotationYaw = false;
-	//bUseControllerRotationRoll = false;
+	BaseLookUpRate = 45.f; 
 
 	// Create a camera boom (pulls in towards the player if there is a collision)
 	mCameraSpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("mCameraSpringArm"));
 	mCameraSpringArm->SetupAttachment(RootComponent);
-	mCameraSpringArm->TargetArmLength = 300.0f; // The camera follows at this distance behind the character	
-	mCameraSpringArm->SocketOffset = FVector(0.0f, 0.0f, 60.0f);
+	mCameraSpringArm->TargetArmLength = 1200.0f; // The camera follows at this distance behind the character	
+	mCameraSpringArm->SocketOffset = FVector(0.0f, 0.0f, 0.0f);
 	mCameraSpringArm->bEnableCameraLag = false; // Do not allow camera to lag
 	mCameraSpringArm->CameraLagSpeed = 15.0f;
 	mCameraSpringArm->bUsePawnControlRotation = false; // Rotate the arm based on the controller
@@ -51,23 +44,40 @@ ABirdPawn::ABirdPawn()
 	mCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
 
 	// Set handling parameters
-	Acceleration = 500.f;
+	Acceleration = 500.0f;
 	TurnSpeed = 50.f;
-	MaxSpeed = 4000.f;
-	MinSpeed = 500.f;
+	MaxSpeed = 6000.f;
+	LandSpeed = 250.f;
 	CurrentForwardSpeed = 500.f;
 }
 
 void ABirdPawn::Tick(float DeltaSeconds)
 {
 	const FVector LocalMove = FVector(CurrentForwardSpeed * DeltaSeconds, 0.f, 0.f);
+	// Get current actor rotation
+	FRotator CurrentRotation = GetActorRotation();
+	// For interpolation from extreme angles
+	FRotator NewRotation;
 
 	// Move plan forwards (with sweep so we stop when we collide with things)
 	AddActorLocalOffset(LocalMove, true);
 
 	// Calculate change in rotation this frame
 	FRotator DeltaRotation(0, 0, 0);
-	DeltaRotation.Pitch = CurrentPitchSpeed * DeltaSeconds;
+	// Check if bird is flying too close too steeply or at obtuse angle downwards
+	if (CurrentRotation.Pitch < 40.0f && CurrentRotation.Pitch > -40.0f) {
+		DeltaRotation.Pitch = CurrentPitchSpeed * DeltaSeconds;
+	}
+	else if (CurrentRotation.Pitch > 30.0f) {
+		NewRotation = CurrentRotation;
+		NewRotation.Pitch = 30.0f;
+		SetActorRotation(FMath::RInterpTo(CurrentRotation, NewRotation, GetWorld()->GetDeltaSeconds(), 0.25f));
+	}
+	else if (CurrentRotation.Pitch < -30.0f) {
+		NewRotation = CurrentRotation;
+		NewRotation.Pitch = -30.0f;
+		SetActorRotation(FMath::RInterpTo(CurrentRotation, NewRotation, GetWorld()->GetDeltaSeconds(), 0.25f));
+	}
 	DeltaRotation.Yaw = CurrentYawSpeed * DeltaSeconds;
 	DeltaRotation.Roll = CurrentRollSpeed * DeltaSeconds;
 
@@ -97,38 +107,27 @@ void ABirdPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 	PlayerInputComponent->BindAxis("Thrust", this, &ABirdPawn::ThrustInput);
 	PlayerInputComponent->BindAxis("MoveUp", this, &ABirdPawn::MoveUpInput);
 	PlayerInputComponent->BindAxis("MoveRight", this, &ABirdPawn::MoveRightInput);
-
-	//// We have 2 versions of the rotation bindings to handle different kinds of devices differently
-	//// "turn" handles devices that provide an absolute delta, such as a mouse.
-	//// "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
-	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
-	PlayerInputComponent->BindAxis("TurnRate", this, &ABirdPawn::TurnAtRate);
-	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
-	PlayerInputComponent->BindAxis("LookUpRate", this, &ABirdPawn::LookUpAtRate);
-}
-
-void ABirdPawn::TurnAtRate(float Rate)
-{
-	// calculate delta for this frame from the rate information
-	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
-}
-
-void ABirdPawn::LookUpAtRate(float Rate)
-{
-	// calculate delta for this frame from the rate information
-	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
 }
 
 void ABirdPawn::ThrustInput(float Val)
 {
+	// Acceleration force
+	float fAcc;
+	if (CurrentForwardSpeed < 500.0)
+	{
+		fAcc = 0.5;
+	}
+	else {
+		fAcc = -0.5f;
+	}
 	// Is there any input?
 	bool bHasInput = !FMath::IsNearlyEqual(Val, 0.f);
 	// If input is not held down, reduce speed
-	float CurrentAcc = bHasInput ? (Val * Acceleration) : (-0.5f * Acceleration);
+	float CurrentAcc = bHasInput ? (Val * Acceleration) : (fAcc * Acceleration);
 	// Calculate new speed
 	float NewForwardSpeed = CurrentForwardSpeed + (GetWorld()->GetDeltaSeconds() * CurrentAcc);
 	// Clamp between MinSpeed and MaxSpeed
-	CurrentForwardSpeed = FMath::Clamp(NewForwardSpeed, MinSpeed, MaxSpeed);
+	CurrentForwardSpeed = FMath::Clamp(NewForwardSpeed, LandSpeed, MaxSpeed);
 }
 
 void ABirdPawn::MoveUpInput(float Val)
@@ -137,7 +136,7 @@ void ABirdPawn::MoveUpInput(float Val)
 	float TargetPitchSpeed = (Val * TurnSpeed * -1.f);
 
 	// When steering, we decrease pitch slightly
-	TargetPitchSpeed += (FMath::Abs(CurrentYawSpeed) * -0.2f);
+	TargetPitchSpeed += (FMath::Abs(CurrentYawSpeed));
 
 	// Smoothly interpolate to target pitch speed
 	CurrentPitchSpeed = FMath::FInterpTo(CurrentPitchSpeed, TargetPitchSpeed, GetWorld()->GetDeltaSeconds(), 2.f);
@@ -156,8 +155,20 @@ void ABirdPawn::MoveRightInput(float Val)
 
 	// If turning, yaw value is used to influence roll
 	// If not turning, roll to reverse current roll value.
-	float TargetRollSpeed = bIsTurning ? (CurrentYawSpeed * 0.5f) : (GetActorRotation().Roll * -2.f);
+	float TargetRollSpeed = bIsTurning ? (CurrentYawSpeed * 0.0f) : (GetActorRotation().Roll * -2.f);
 
 	// Smoothly interpolate roll speed
 	CurrentRollSpeed = FMath::FInterpTo(CurrentRollSpeed, TargetRollSpeed, GetWorld()->GetDeltaSeconds(), 2.f);
+
+	//if ((Controller != NULL) && (Val != 0.0f))
+	//{
+	//	// find out which way is right
+	//	const FRotator Rotation = Controller->GetControlRotation();
+	//	const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+	//	// get right vector 
+	//	const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	//	// add movement in that direction
+	//	AddMovementInput(Direction, Val);
+	//}
 }
--- Source/SundownPrototype/BirdPawn.h
+++ Source/SundownPrototype/BirdPawn.h
@@ -10,7 +10,7 @@
 #include "BirdPawn.generated.h"
 
 UCLASS()
-class SUNDOWNPROTOTYPE_API ABirdPawn : public APawn
+class SUNDOWNPROTOTYPE_API ABirdPawn : public ACharacter
 {
 	GENERATED_BODY()
 
@@ -35,18 +35,6 @@ public:
 		float BaseLookUpRate;
 
 protected:
-	/**
-	* Called via input to turn at a given rate.
-	* @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	*/
-	void TurnAtRate(float Rate);
-
-	/**
-	* Called via input to turn look up/down at a given rate.
-	* @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	*/
-	void LookUpAtRate(float Rate);
-
 	/** Bound to the thrust axis */
 	void ThrustInput(float Val);
 
@@ -77,7 +65,7 @@ private:
 
 	/** Min forward speed */
 	UPROPERTY(Category = Yaw, EditAnywhere)
-		float MinSpeed;
+		float LandSpeed;
 
 	/** Current forward speed */
 	float CurrentForwardSpeed;
