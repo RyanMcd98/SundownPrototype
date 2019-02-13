47bf9895c815b0244e451fd7e523c966b9b0b0d1
DESKTOP-A8N2MRH\scott <scottmichaeldouglas@gmail.com>
2019-02-13 21:41:01 +0000

    Basic brazier system working

--- Source/SundownPrototype/MyTriggerVolume.h
+++ Source/SundownPrototype/MyTriggerVolume.h
@@ -6,6 +6,7 @@
 #include "GameFramework/Actor.h"
 #include "Runtime/LevelSequence/Public/LevelSequence.h"
 #include "Runtime/LevelSequence/Public/LevelSequencePlayer.h"
+#include "GameFramework/Pawn.h"
 #include "MyTriggerVolume.generated.h"
 
 UCLASS(Blueprintable)
@@ -24,13 +25,42 @@ public:
 
 	// overlap begin function
 	UFUNCTION()
-		void OnOverlapBegin(class AActor* OverlappedActor, class AActor* OtherActor);
+	void OnOverlapBegin(class AActor* OverlappedActor, class AActor* OtherActor);
 
 	// overlap end function
 	UFUNCTION()
-		void OnOverlapEnd(class AActor* OverlappedActor, class AActor* OtherActor);
+	void OnOverlapEnd(class AActor* OverlappedActor, class AActor* OtherActor);
 
-	//Level sequence asset played when pawn enters trigger box
-	UPROPERTY(EditAnywhere)
+	UFUNCTION()
+	void SitOnBrazier();
+
+	//Reference to player
+	TArray<AActor*> Pawns;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Pawn)
+	TSubclassOf<class AActor>  PawnClassType;
+	APawn* Pawn;
+
+	//Level sequence player used to play fade out
+	UPROPERTY()
+	ULevelSequencePlayer* SequencePlayer;
+
+	//Actor used to play fade out
+	UPROPERTY()
+	ALevelSequenceActor* SequenceActor;
+
+	//Level sequence asset played when pawn enters trigger box, can be set in trigger box details 
+	UPROPERTY(EditAnywhere, Category = SequenceToPlay)
 	class ULevelSequence* FadeOut;
+
+	//New location which the player is set to when pawn enters trigger box, can be set in trigger box details
+	UPROPERTY(EditAnywhere, Category = NewLocation)
+	FVector Location;
+
+	//New rotation which the player is set to when pawn enters trigger box, can be set in trigger box details
+	UPROPERTY(EditAnywhere, Category = NewRotation)
+	FRotator Rotation;
+
+	//Set location timer handle
+	UPROPERTY()
+	FTimerHandle TimerHandle;
 };
